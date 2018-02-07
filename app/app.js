a2a4aa5bbe1bff201927bcf72af1e379dfa0c31b
Christopher Fox <cfox1290@gmail.com>
2018-02-07 14:33:30 -0700

    finished assignment

--- app/app.js
+++ app/app.js
@@ -3,7 +3,27 @@ angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider,
     .state('home',{
       url:'/',
       templateUrl: "app/about/aboutTmpl.html"
-    });
+    }).state('packages',{
+      url:'/packages',
+      templateUrl: 'app/packages/packagesTmpl.html',
+      controller:'packagesCtrl'
+    }).state('locations',{
+      url:'/locations',
+      templateUrl: 'app/locations/locationsTmpl.html',
+      controller: 'locationsCtrl'
+    }).state('booked/:id',{
+      url:'/booked',
+      templateUrl: 'app/booked/bookedTmpl.html',
+      controller:'bookedCtrl'
+    }).state('adventurers',{
+      url:'/adventurers',
+      templateUrl: 'app/about/adventurers/adventurersTmpl.html',
+      parent: 'home'
+    }).state('contact',{
+      url:'/contact',
+      templateUrl: 'app/contact/contactTmpl.html',
+      parent:'home'
+    })
 
   $urlRouterProvider
     .otherwise('/');
